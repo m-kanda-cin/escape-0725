259b5ebd5745b62b8b60626ac2aed827faaca638
m-kanda-cin <m.kanda.cingroup2012@gmail.com>
2025-06-23 17:24:19 +0900

    テスト用サンプルクラス ダミーファイル

---
+++ java/TestSample.java
@@ -0,0 +1,6 @@
+public class TestSample {
+    public static void main(String[] args) {
+        // これはテスト用のサンプルクラスです。実際の処理はありません。
+        System.out.println("This is a legacy test class.");
+    }
+}
---
+++ src/old_script.php
@@ -0,0 +1,9 @@
+<?php
+// これは昔の古いスクリプトです。現在は使われていません。
+
+function sayHello() {
+    echo "Hello, world!";
+}
+
+// テスト用に関数を呼び出す
+sayHello();
